f23a736faf622c3d42d55a7fa6b23ac47fdc783b
Jose Quizon <josequizon@jcqfolio.com>
2020-07-31 02:26:58 -0400

    'updated_existingemp_dta_edit'

--- edb.py
+++ edb.py
@@ -13,10 +13,12 @@ from newEmpdataFuncsModule import hiredate
 from newEmpdataFuncsModule import depsetter
 from newEmpdataFuncsModule import paysetter
 from newEmpdataFuncsModule import verifier
+from newEmpdataFuncsModule import linechooser
 from savers import readytosave
 from savers import filencrypter
 from savers import listcreate
 from savers import newempfilesaver
+from editEmp import empsearchprinter
 
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Openloader vars
@@ -31,7 +33,7 @@ dataentered=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>time variables for time based funcs
 import datetime
 z=datetime.datetime.now()
-monthnow = int((z.strftime("%m")))
+monthnow = (z.strftime("%m"))
 daynow = int((z.strftime("%d")))
 yearnow = int((z.strftime("%Y")))-2000
 print(monthnow)
@@ -48,6 +50,49 @@ sickdatestart =[]
 persdatestart =[]
 vacdatestart =[]
 
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> position of input match on employeesearch
+searchpos=[0]
+
+# this fnction is for editing existing employee info entries
+def EditempInfo():
+    print(searchpos)
+    toedit = input('enter the no. of the line you wish to edit or (s) if you want to save youre done and ready to save: ')
+    if toedit == '<':
+        modeset()
+    elif toedit == 's':
+        editlist = nocmemlist[searchpos[0]]
+        editlistA = nocmemlist[searchpos[0]+1]
+        editlistB = nocmemlist[searchpos[0]+2]
+        editlistC = nocmemlist[searchpos[0]+3]
+        readytosave(editlist,editlistA,editlistB,editlistC,EditempInfo,modeset,newempfilesaver,noclist,monthnow,daynow,yearnow,depset)
+    elif int(toedit)<18:
+        linechooser(toedit,noclist,nocmemlist,searchpos,modeset,monthnow,daynow,yearnow,depset,EditempInfo)
+    else:
+        print('boopbeep doesnot compute')
+        EditempInfo()
+
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+
+
+
+
+
+
+
+# this fnction is for new employee info entrie
 def Ndataenter():
     firstset(dataentered,modeset)
     #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>first name            
@@ -68,9 +113,9 @@ def Ndataenter():
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>sets position  
     paysetter(dataentered)   
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>sets pay
-    verifier(dataentered,sickdatestart,persdatestart,vacdatestart,modeset,monthnow,daynow,yearnow,depset)
+    verifier(dataentered,sickdatestart,persdatestart,vacdatestart,modeset,monthnow,daynow,yearnow,depset,noclist)
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>confirm or edit info
-    readytosave(dataentered,sickdatestart,persdatestart,vacdatestart,verifier,modeset,newempfilesaver,noclist)
+    readytosave(dataentered,sickdatestart,persdatestart,vacdatestart,verifier,modeset,newempfilesaver,noclist,monthnow,daynow,yearnow,depset)
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> say yes and runs func to creates files --> encrypted --> saved
 
 
@@ -78,14 +123,7 @@ def Ndataenter():
 
 
 
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -108,13 +146,17 @@ def Ndataenter():
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>mode select
 def modeset():
+    noclist.clear()
+    nocmemlist.clear()
+
     employeeloader(noclist,nocmemlist)
     mission = input("1 for entry or 2 for output: ")
     if mission== '1':
         print("A new employee! coo'coo :)")
         Ndataenter()
     elif mission== '2':
-        paysetter(dataentered)
+        empsearchprinter(noclist,nocmemlist,searchpos,modeset)
+        EditempInfo()
     elif mission == '3':
         print(nocmemlist)
         print(noclist)
