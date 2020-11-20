6bad6a0c6876599ac14da3234ccbc70da617fe6c
Jose Quizon <josequizon@jcqfolio.com>
2020-11-20 08:11:09 -0500

    multi-employee_file_creator

---
+++ emprostercreator.py
@@ -0,0 +1,112 @@
+noclist = ['0','jasonbourne','sarawalker', 'johncasey', 'charlesbartowski','jackbauer','tonyroop','gregcosent','macgregorjones','jalenhurts','adamdriver','daisyridley','alecbaldwin','stefftuan','chrisrudolf','kyloren','michaelokeyo','michaeldonaldson','petercamp','justinedean','marybailey','marcsinger','francofranus','chrisvelez','josequizon','jumpasmith','jumpajones','thavonepengpang','paulsingh','jerryspringer','andrewjones','colehenderson','georgest.pierre','lucpetty','toddbryant','robinbanger']
+
+
+nocmemlist = [[0],[0],[0],[0],['jason', 'bourne', 'jabo', '987654321', '9876543211', '1 ultimatum st', 'helena', 'MT', '54321', '12', '08', '19', 'acct', 'acctnt', '31.25', '1250.0', '5000.0', '65000.0', '3', '-3', '2', '-2', '2', '-2','0','0'], ['1/2/20', '1/3/20', '1/4/20'], ['2/2/20', '2/3/20'], ['3/3/20', '3/4/20'],['sara', 'walker', 'yvonne', '123456789', '1234567890', '1 Bartowski lane', 'burbank', 'CA', '54321', '08', '02', '15', 'FD', 'fdagnt', '24.03846153846154', '961.5384615384615', '3846.153846153846', '50000.0', '1', '3', '4', '2', '4', '2','4'], ['0', '1/2/20'], ['0', '2/1/20', '2/2/20', '2/3/20', '2/4/20'], ['0', '4/1/20', '4/2/20', '4/3/20', '4/5/20'], ['john', 'casey', 'jc', '999999999', '1111111111', '2 buymoria blvd', 'burbank', 'CA', '44444', '03', '04', '17', 'FD', 'fdagnt', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'], ['charles', 'bartowski', 'chuck', '000000000', '9999999999', '2 buymoria lane', 'burbank', 'CA', '66333', '06', '26', '08', 'FD', '*adeptmgr', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','11'], ['0'], ['0'], ['0'],['jack','bauer','jbow','987654321','9876543211','1 ultimatum st','memphis','TN','54321','12','08','19','acct','acctnt','31.25','1250.0','5000.0','65000.0','3','-3','2','-2','2','-2','0','0'],['1/2/20', '1/3/20', '1/4/20'], ['2/2/20', '2/3/20'], ['3/3/20', '3/4/20'],['tony','roop','oldje','987654321','9876543211','1 deacon st','knoxville','TN','54321','01','07','17','acct','*asscntrller','31.25','1250.0','5000.0','65000.0','3','-3','2','-2','2','-2','0','0'],['1/2/20', '1/3/20', '1/4/20'], ['2/2/20', '2/3/20'], ['3/3/20', '3/4/20'], ['greg','cosent','gc','232323232','9876543211','1 dead st','athens','ga','54321','04','12','12','acct','*cntrller','31.25','1250.0','5000.0','65000.0','3','-3','2','-2','2','-2','0','0'],['1/2/20', '1/3/20', '1/4/20'], ['2/2/20', '2/3/20'], ['3/3/20', '3/4/20'],['macgregor','jones','mac','345678912','9876543211','1 Qb st','athens','AL','54321','04','12','12','sle','*res','31.25','1250.0','5000.0','65000.0','3','-3','2','-2','2','-2','0','0'],['1/2/20', '1/3/20', '1/4/20'], ['2/2/20', '2/3/20'], ['3/3/20', '3/4/20'],['jalen','hurts','2','345678912','9876543211','3 Qb st','houston','tx','54321','02','08','12','sle','res','31.25','1250.0','5000.0','65000.0','3','-3','2','-2','2','-2','0','0'],['1/2/20', '1/3/20', '1/4/20'], ['2/2/20', '2/3/20'], ['3/3/20', '3/4/20'],['adam','driver','kylo','345678912','9876543211','3 sith ave','burbank','CA','54321','02','08','12','sle','*deptsup','31.25','1250.0','5000.0','65000.0','3','-3','2','-2','2','-2','0','0'],['1/2/20', '1/3/20', '1/4/20'], ['2/2/20', '2/3/20'], ['3/3/20', '3/4/20'], ['daisy','ridley','dazer','345678912','9876543211','3 tatooine ave','burbank','CA','54321','02','08','12','sle','*deptmgr','31.25','1250.0','5000.0','65000.0','3','-3','2','-2','2','-2','0','0'],['1/2/20', '1/3/20', '1/4/20'], ['2/2/20', '2/3/20'], ['3/3/20', '3/4/20'],['alec', 'baldwin', 'ac', '888999999', '1111111111', '23 red october blvd', 'Los Angeles', 'CA', '44444', '03', '04', '17', 'sle', '*adeptmgr', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['steff', 'tuan', 'Chienne', '888765999', '1111111111', '14 red lingquine blvd', 'brooklyn', 'ny', '49844', '10', '24', '05', 'FD', '*adeptmgr', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['chris', 'rudolf', 'crudo', '8880009999', '1111111111', '14 Assman blvd', 'brooklyn', 'ny', '49844', '11', '28', '04', 'FD', '*adeptmgr', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['kylo', 'ren', 'solob', '8670009999', '1111111111', '14 Sithwalker blvd', 'brooklyn', 'ny', '49844', '11', '28', '04', 'FD', '*deptmgr', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['michael', 'okeyo', 'mikeo', '8880009999', '1111111111', '14 Sithwalker blvd', 'brooklyn', 'ny', '49844', '11', '28', '04', 'FD', 'bllmn', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['michael', 'donaldson', 'mikeD', '8880129999', '1111000111', '14 3rd blvd', 'brooklyn', 'ny', '49844', '10', '28', '14', 'FD', 'bllmn', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['peter', 'camp', 'pete gunn', '8898001299', '1111234561', '14 queens blvd', 'sunnyside', 'ny', '49844', '09', '28', '07', 'FD', 'drmn', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['justine', 'dean', 'jd', '8880001299', '1111111111', '1 roosevelt ave', 'woodside', 'ny', '49844', '03', '28', '17', 'FD', 'fdagnt', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['mary', 'bailey', 'md', '8880001299', '1111111111', '444 roosevelt ave', 'jackson hts', 'ny', '49844', '07', '28', '07', 'FD', 'fdagnt', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['marc', 'singer', 'beastmaster', '3330001299', '1111111111', '123 crescent st', 'lic', 'ny', '49844', '02', '28', '09', 'FD', 'fdagnt', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['franco', 'franus', 'F2', '3330001299', '1111111111', '123 gpt ave', 'greenpoint', 'ny', '49844', '12', '28', '09', 'FD', 'ntaud', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['chris', 'velez', 'unk slam', '3330001299', '8881111111', '123 cbx ave', 'bronx', 'ny', '49844', '08', '18', '14', 'FD', 'ntaud', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['jose', 'quizon', 'chico', '3330001299', '8881111111', '7054 bway ave', 'jax hts', 'ny', '49844', '01', '18', '06', 'FD', '*ntmgr', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['jumpa', 'smith', 'js', '3330001299', '8881111111', '7054 bway ave', 'jax hts', 'ny', '49844', '01', '18', '06', 'hsk', 'hseman', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['jumpa', 'jones', 'jj', '3330001299', '8881111111', '7054 bway ave', 'jax hts', 'ny', '49844', '01', '18', '06', 'hsk', 'hseman', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['thavone', 'pengpang', 'tp', '3330001299', '8881111111', '7054 bway ave', 'jax hts', 'ny', '49844', '05', '30', '06', 'hsk', 'hseman', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['paul', 'singh', 'paulie', '3330001299', '8881111111', '754 bway ave', 'jax hts', 'ny', '49844', '01', '30', '06', 'hsk', 'hskper', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['jerry', 'springer', 'jeeeerrrry', '3330001299', '8881111111', '705 bway ave', 'jax hts', 'ny', '49844', '06', '20', '08', 'hsk', 'hskper', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['andrew', 'jones', 'aj', '3330001299', '8881111111', '7 bway ave', 'jax hts', 'ny', '49844', '06', '14', '08', 'hsk', 'hskper', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['cole', 'henderson', 'hendo', '3330001299', '8881111111', '7054 bway ave', 'jax hts', 'ny', '49844', '01', '24', '18', 'pbx', 'oprtr', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['george', 'st.pierre', 'gsp', '3330001299', '8881111111', '7054 bway ave', 'jax hts', 'ny', '49844', '01', '24', '18', 'eng', 'engnr', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['luc', 'petty', 'LP', '3330001299', '8881111111', '7 roosevelt ave', 'bronx', 'ny', '49844', '01', '24', '18', 'eng', 'engnr', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['todd', 'bryant', 'toddB', '3330001299', '8881111111', '290 roosevelt ave', 'brooklyn', 'ny', '49844', '03', '14', '18', 'eng', 'engnr', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0'],['robin', 'banger', 'rB', '3330001299', '8881111111', '290 greenpt ave', 'brooklyn', 'ny', '11111', '05', '14', '18', 'pbx', 'oprtr', '25.0', '1000.0', '4000.0', '48000.0', '0', '0', '0', '0', '0', '0','2'], ['0'], ['0'], ['0']]
+
+nocadder=[]
+
+def listcreate(list,list2,list3,list4,nlist):
+#     this creates txt files from list
+    empname = nlist
+    with open(f'testdocs\\noclist.night',mode='w')as n:
+        for m in range(len(nocadder)):
+            n.write(f'{nocadder[m]}\n')    
+    with open(f'testdocs\\{empname}.night',mode='w')as t:
+        for k in range(len(list)):
+            t.write(f'{list[k]}\n')
+    with open(f'testdocs\\{empname}sickdates.night',mode='w')as q:
+        for r in range(len(list2)):
+            q.write(f'{list2[r]}\n')
+    with open(f'testdocs\\{empname}persdates.night',mode='w')as s:
+        for y in range(len(list3)):
+            s.write(f'{list3[y]}\n')
+    with open(f'testdocs\\{empname}vacdates.night',mode='w')as h:
+        for j in range(len(list4)):
+            h.write(f'{list4[j]}\n')
+    print('files created!')
+
+
+
+
+
+def filencrypter(listo):  
+#         from cryptography.fernet import Fernet
+#         key = Fernet.generate_key()
+#         fkey = open('testdocs/filekey.night','wb')
+#         fkey.write(key)
+#         print (key)
+#     this open encrypter key created by keycreate() as variable
+
+    empname = listo
+#  this is the encryption key opening
+    from cryptography.fernet import Fernet
+    fkey = open('testdocs/filekey.night','rb')
+    key = fkey.read()
+    print (key)
+    cipher = Fernet(key)
+
+#     opens created files to be encrypted and encrypts
+    Nocfile= 'testdocs/noclist.night'
+    with open(Nocfile,'rb')as e:
+        Nocfiletoencrypt = e.read()
+
+        
+    Nocencryptedfile = cipher.encrypt(Nocfiletoencrypt) 
+    with open(Nocfile,'wb') as ee:
+        ee.write(Nocencryptedfile)
+
+# >>>>>>>>>>>>>>>>>>>>>
+    infofile= 'testdocs/'+ empname +'.night'
+    with open(infofile,'rb')as f:
+        filetoencrypt = f.read()
+
+
+    encryptedfile = cipher.encrypt(filetoencrypt) 
+    with open(infofile,'wb') as ef:
+        ef.write(encryptedfile)
+
+
+# >>>>>>>>>>>>>>>>>>>>>          
+    sickfile= 'testdocs/'+ empname +'sickdates.night'
+    with open(sickfile,'rb')as ff:
+        sickfiletoencrypt = ff.read()
+
+    sickencryptedfile = cipher.encrypt(sickfiletoencrypt) 
+    with open(sickfile,'wb') as eff:
+        eff.write(sickencryptedfile)
+
+
+# >>>>>>>>>>>>>>>>>>>>>          
+    persfile= 'testdocs/'+ empname +'persdates.night'
+    with open(persfile,'rb')as fff:
+        persfiletoencrypt = fff.read()
+
+    persencryptedfile = cipher.encrypt(persfiletoencrypt) 
+    with open(persfile,'wb') as efff:
+        efff.write(persencryptedfile)
+
+
+# >>>>>>>>>>>>>>>>>>>>>      
+    vacfile= 'testdocs/'+ empname +'vacdates.night'
+    with open(vacfile,'rb')as ffff:
+        vacfiletoencrypt = ffff.read()
+
+    vacencryptedfile = cipher.encrypt(vacfiletoencrypt) 
+    with open(vacfile,'wb') as effff:
+        effff.write(vacencryptedfile)
+
+        
+    with open('testdocs/'+ empname +'.night','rb') as df:
+        encryptedfile = df.read()
+        print(encryptedfile)
+        print('>>>>>>>>>>>>>>>>>>>>>>>')
+    decrypted_file = cipher.decrypt(encryptedfile)
+    print(decrypted_file.decode())      
+
+
+
+
+for x in range(len(noclist)):
+		nocadder.append(noclist[x])
+		listcreate(nocmemlist[(x*4)],nocmemlist[(x*4)+1],nocmemlist[(x*4)+2],nocmemlist[(x*4)+3],noclist[x])
+		filencrypter(noclist[x])
