b164c81dc1544ece0503d5fc584a6c879f6ba070
Jose Quizon <josequizon@jcqfolio.com>
2020-11-29 07:30:30 -0500

    groupVIewer_functions_integrated

--- mainInstant.py
+++ mainInstant.py
@@ -1,3 +1,5 @@
+
+
 import datetime
 z=datetime.datetime.now()
 monthnow = int((z.strftime("%m")))
@@ -8,7 +10,8 @@ print(yearnow)
 print(yearint)
 
 # zipped dict
-obdict = 0
+obdictA = []
+
 noclistkey=[]
 dictionaryhold = []
 
@@ -335,9 +338,8 @@ def instantiation(nlist,nmlist):
     for x in range(len(dictionaryhold)):
         noclistkey.append(dictionaryhold[x]['first']+dictionaryhold[x]['last'])
     print(noclistkey)
-    obdict=dict(zip(noclistkey,nlist))
-
-
+    obs = dict(zip(noclistkey,nlist))
+    obdictA.append(obs)
 # ignition func --- this starts instantiation 
 def OOPstart(nlist,nmlist):
     instantiation(nlist,nmlist)
@@ -438,10 +440,422 @@ def quickempcheckMenu(nlist,retfunc):
 
 
 
+# this is where the departments and positions are divided up
+# this starts with choice 2 on objectmMenu()
+
+obdict = 0
+fddict = {}
+fdMgmt = {}
+fdagnt = {}
+ntaud = {}
+bllmn = {}
+drmn = {}
+FDposDict = [fdagnt,ntaud,bllmn,drmn]
+FDposDictSTR = ['fdagnt','ntaud','bllmn','drmn']
+
+
+hskdict = {}
+hskMgmt = {}
+hskper = {}
+hseman ={} 
+HskposDict = [hskper,hseman]
+HskposDictSTR = ['hskper','hseman']
+
+sledict = {}
+sleMgmt = {}
+res = {}
+SleposDict = [res]
+SleposDictSTR = ['res']
+
+pbxdict = {}
+pbxMgmt = {}
+oprtr = {}
+pbxposDict = [oprtr]
+pbxposDictSTR = ['oprtr']
+
+accntdict = {}
+accntMgmt = {}
+acct = {}
+accntposDict = [acct]
+accntposDictSTR = ['acct']
+
+engdict = {}
+engMgmt = {}
+engnr = {}
+engposDict = [engnr]
+engposDictSTR = ['engnr']
+
+positlist = [fdagnt,ntaud,bllmn,drmn,hskper,hseman,res,oprtr,acct,engnr]
+positlistSTR = ['front desk agents','nightauditors','bellmen','doormen','housekeeper','housemen','reservationists','operators','accountants','engineers']
+
+mngmt = [accntMgmt,engMgmt,fdMgmt,hskMgmt,pbxMgmt,sleMgmt]
+mngmtString = ['Accounting Mgmt','Engineering Mgmt','Front Desk Mgmt','Housekeeping Mgmt','Reservations Mgmt','Sales Mgmt']
+
+dep = [hskdict,fddict,engdict,pbxdict,sledict,accntdict]                
+depString = ['housekeeping','front desk','engineering','pbx','sales','accounting'] 
+
+positSalcollectY = []
+positSalcollectM = []
+positSalcollectW = []
+
+
+
+# this function sorts the whole direct by last name. loops thru that and updates dep dict. then loops throug dep dict
+# and updates position dictionaries from that dep
+def depsep(dep,dest,destA,destAstr,destB):
+    obd={key: value for key, value in sorted(obdictA[0].items(), key=lambda item: item[1].last)}
+    for x in obd:
+        if obd[x].dept == dep:
+            dest.update({obd[x].first+obd[x].last:obd[x]})  
+        else:
+            pass
+    for y in dest:
+        if dest[y].position[0] == '*':
+            destB.update({dest[y].first+dest[y].last:dest[y]})
+        else:
+            for b in range (len(destA)):
+                if dest[y].position == destAstr[b]:
+                    destA[b].update({dest[y].first+dest[y].last:dest[y]})
+                else:
+                    pass
+    print('directory created')
+    
+        
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+#  subfunc to run to return user to groupfuncmenu
+def subretfunc(prevfunc,nlist,retfunc):
+    print(f'\nWhat would you like to do now?\n')
+    subChoice = input(f'\nTry Again enter ... <y>\nGo back to the previous Menu enter ... <*>\nGo back to the Viewer Menu enter ... <m>\n:')
+    if subChoice == 'y':
+        prevfunc(nlist,retfunc)
+    elif subChoice == '*':
+        groupfuncMenu(nlist,retfunc)
+    elif subChoice == 'm':
+        objectMenu(nlist,retfunc)
+    else: 
+        print('sorry does not compute...try again')
+        subretfunc(prevfunc,nlist,retfunc)
+
+# this function shows y m w -ly payroll per dept, their members, sic per vac remaining and taken, yrs of experience
+
+def depStat(nlist,retfunc):
+    depkeyhold=[]
+    depkeyholdSalY=[]
+    depkeyholdSalW=[]
+    depkeyholdSalM=[]
+
+    siclist = []
+    perlist = []
+    vaclist = []
+
+    sictlist = []
+    pertlist = []
+    vactlist = []
+
+    explist = []
+
+    mnglist = []
+    lbrlist = []
+    
+    for depS in range(len(depString)):
+        print(f'{depS}....{depString[depS]}')
+    
+    depchoiceno = input('please choose the department you would like to review: ')
+    if depchoiceno in ('012345') and  len(depchoiceno)>0:
+        for z in dep[int(depchoiceno)].keys():
+                depkeyhold.append(z)
+        for x in range(len(depkeyhold)):
+            depkeyholdSalY.append(int(float(obdictA[0][depkeyhold[x]].yearlypay)))    
+            depkeyholdSalM.append(int(float(obdictA[0][depkeyhold[x]].monthlypay)))    
+            depkeyholdSalW.append(int(float(obdictA[0][depkeyhold[x]].weeklypay)))    
+            siclist.append(int(float(obdictA[0][depkeyhold[x]].sickremaining)))
+            perlist.append(int(float(obdictA[0][depkeyhold[x]].persremaining)))
+            vaclist.append(int(float(obdictA[0][depkeyhold[x]].vacremaining)))
+            sictlist.append(int(float(obdictA[0][depkeyhold[x]].sicktaken)))
+            pertlist.append(int(float(obdictA[0][depkeyhold[x]].perstaken)))
+            vactlist.append(int(float(obdictA[0][depkeyhold[x]].vactaken)))
+            explist.append(int(float(obdictA[0][depkeyhold[x]].empnoyrs)))
+            
+            if obdictA[0][depkeyhold[x]].position[0]=='*':
+                mnglist.append(obdictA[0][depkeyhold[x]].first+' '+ obdictA[0][depkeyhold[x]].last + '//' + obdictA[0][depkeyhold[x]].position)
+            else:
+                lbrlist.append(obdictA[0][depkeyhold[x]].first+' '+ obdictA[0][depkeyhold[x]].last + '//' + obdictA[0][depkeyhold[x]].position)
+
+
+            
+
+        print('\nManagers:')
+        for mm in range (len(mnglist)):
+            print(f'{mnglist[mm]}\n')
+        print('\nTeam Members:')   
+        for ll in range (len(lbrlist)):
+            print(f'{lbrlist[ll]}\n' )
+        
+        print(f'sum total years of worker experience:....{sum(explist)}\n')
+#         print(f'{depString[int(userChoE)]}....total personnel count: {len(depStatdict)}\n')
+        print(f'Yearly Payroll total....{sum(depkeyholdSalY)}\n')
+        print(f'Monthly Payroll total....{sum(depkeyholdSalM)}\n')
+        print(f'Weekly Payroll total....{sum(depkeyholdSalW)}\n')
+        
+        print(f'total no. of sick days taken:....{sum(sictlist)} / {sum(siclist)} remaining')
+        print(f'total no. of personal days taken:....{sum(pertlist)} / {sum(perlist)} remaining')
+        print(f'total no. of vacation days taken:....{sum(vactlist)} / {sum(vaclist)} remaining')
+        
+    else:
+        print('sorry try again!')
+        depStat(nlist,retfunc)
+        
+    subretfunc(depStat,nlist,retfunc)
+
+
+# this function prints the directory for the whole property
+def propDirprint(nlist,retfunc):
+    pdp={key: value for key, value in sorted(obdictA[0].items(), key=lambda item: item[1].last)}
+    for  x in pdp:
+        print(f'\n{pdp[x].last}, {pdp[x].first}  <{pdp[x].dept}//{pdp[x].position}>......{pdp[x].contel}\n{pdp[x].address}\n{pdp[x].city}, {pdp[x].state} {pdp[x].zipcode}\n')
+    subretfunc(propDirprint,nlist,retfunc)
+
+def indyDirprint(nlist,retfunc):
+    for pick in range(len(depString)):
+        print(f'{pick}....{depString[pick]}')
+    
+    usercho = input('choose the no. of the dep directory you would like to see: ')
+
+    if usercho not in ('012345') and len(usercho)>0:
+        print('try again')
+        indyDirprint(nlist,retfunc)
+    else:
+        for ent in dep[int(usercho)]:
+            print(f'\n{dep[int(usercho)][ent].last}, {dep[int(usercho)][ent].first}  <{dep[int(usercho)][ent].dept}//{dep[int(usercho)][ent].position}>......{dep[int(usercho)][ent].contel}\n{dep[int(usercho)][ent].address}\n{dep[int(usercho)][ent].city}, {dep[int(usercho)][ent].state} {dep[int(usercho)][ent].zipcode}\n')
+    
+    subretfunc(indyDirprint,nlist,retfunc)        
+
+
+
+# this function prints the directory for a chosen labor position // or the management direct
+def specDirprint(nlist,retfunc):
+    for pos in range(len(positlistSTR)):
+        print(f'{pos}....{positlistSTR[pos]}')
+    print(f'mg....Manager directory')
+    userchoA = input('choose the no. of the position directory you would like to see: ')
+    if userchoA == 'mg':
+        for dmg in range(len(mngmt)):
+            for subdmg in mngmt[dmg]:
+                print(f'\n{mngmt[dmg][subdmg].last}, {mngmt[dmg][subdmg].first}  <{mngmt[dmg][subdmg].dept}//{mngmt[dmg][subdmg].position}>......{mngmt[dmg][subdmg].contel}\n{mngmt[dmg][subdmg].address}\n{mngmt[dmg][subdmg].city}, {mngmt[dmg][subdmg].state} {mngmt[dmg][subdmg].zipcode}\n')
+    elif userchoA not in ('0123456789m') or len(userchoA) == 0 :
+        print('try again')
+        specDirprint(nlist,retfunc)
+    else:
+        for pst in positlist[int(userchoA)]:
+            print(f'\n{positlist[int(userchoA)][pst].last}, {positlist[int(userchoA)][pst].first}  <{positlist[int(userchoA)][pst].dept}//{positlist[int(userchoA)][pst].position}>......{positlist[int(userchoA)][pst].contel}\n{positlist[int(userchoA)][pst].address}\n{positlist[int(userchoA)][pst].city}, {positlist[int(userchoA)][pst].state} {positlist[int(userchoA)][pst].zipcode}\n')
+    subretfunc(specDirprint,nlist,retfunc)
+
+# prints total dep sal by yr mo wk -- ly
+def depsalprint(nlist,retfunc):
+    salcollectY = []
+    salcollectM = []
+    salcollectW = []
+    
+    for depick in range(len(depString)):
+        print(f'{depick}....{depString[depick]}')
+    print
+    
+    userchoC = input('choose the department salary you would like to see: ')
+
+    
+    if userchoC in ('012345') and len(userchoC)>0:
+        chosendict = dep[int(userchoC)]
+        for cc in chosendict:
+            salcollectY.append(float(chosendict[cc].yearlypay))
+            salcollectM.append(float(chosendict[cc].monthlypay))
+            salcollectW.append(float(chosendict[cc].weeklypay))
+            
+        print(f'{depString[int(userchoC)]} department total yearly salary\n{sum(salcollectY)}\n')
+        print(f'{depString[int(userchoC)]} department total monthly salary\n{sum(salcollectM)}\n')
+        print(f'{depString[int(userchoC)]} department total weekly salary\n{sum(salcollectW)}\n')
+        subretfunc(depsalprint,nlist,retfunc)
+    else:
+        print('Please try again')
+        depsalprint(nlist,retfunc)
 
 
 
 
+# this func prints the count of each position 
+def positCT(nlist,retfunc):
+    for ct in range(len(positlistSTR)):
+        print(f'{positlistSTR[ct]}....{len(positlist[ct])}')
+    
+    for ctA in range(len(mngmtString)):
+        print(f'{mngmtString[ctA]}....{len(mngmt[ctA])}')
+    subretfunc(positCT,nlist,retfunc)
+
+# this function only runs inside positionsalprint func. it provides submenu for printing  spcific dep mgr sals
+def mgmtpositsal(nlist,retfunc):
+    for mgsal in range(len(mngmtString)):
+        print(f'{mgsal}....{mngmtString[mgsal]}')
+        
+    print(f'6....All')
+
+    userchoDmg = input('Please choose an option: ')
+
+    if userchoDmg in ('012345') and len(userchoDmg)>0 and (len(mngmt[int(userchoDmg)])) > 0:
+        MgChospositdict = mngmt[int(userchoDmg)]
+        for posal in MgChospositdict:
+            positSalcollectY.append(float(MgChospositdict[posal].yearlypay))
+            positSalcollectM.append(float(MgChospositdict[posal].monthlypay))
+            positSalcollectW.append(float(MgChospositdict[posal].weeklypay))
+        print(f'{mngmtString[int(userchoDmg)]}....total count:{len(MgChospositdict)}')
+        yearlyAVG = float((sum((positSalcollectY))) // (len(positSalcollectY)))
+        monthlyAVG = float((sum((positSalcollectM))) // (len(positSalcollectM)))
+        weeklyAVG = float((sum((positSalcollectW))) // (len(positSalcollectW)))   
+        print(f'Yearly total....{sum(positSalcollectY)}\nYearly Avg....{yearlyAVG}\n')
+        print(f'Monthly total....{sum(positSalcollectM)}\nMonthly Avg....{monthlyAVG}\n')
+        print(f'Weekly total....{sum(positSalcollectW)}\nWeekly Avg....{weeklyAVG}\n')
+        positSalcollectY.clear()
+        positSalcollectM.clear()
+        positSalcollectW.clear()
+        subretfunc(mgmtpositsal,nlist,retfunc)
+    elif userchoDmg == '6'and len(userchoDmg)>0:
+        for mgposal in range(len(mngmt)):
+            for subposal in mngmt[mgposal]:
+                positSalcollectY.append(float(mngmt[mgposal][subposal].yearlypay))
+                positSalcollectM.append(float(mngmt[mgposal][subposal].monthlypay))
+                positSalcollectW.append(float(mngmt[mgposal][subposal].weeklypay))
+            yearlyAVG = float((sum((positSalcollectY))) // (len(positSalcollectY)))
+            monthlyAVG = float((sum((positSalcollectM))) // (len(positSalcollectM)))
+            weeklyAVG = float((sum((positSalcollectW))) // (len(positSalcollectW)))   
+            print(f'Yearly total....{sum(positSalcollectY)}\nYearly Avg....{yearlyAVG}\n')
+            print(f'Monthly total....{sum(positSalcollectM)}\nMonthly Avg....{monthlyAVG}\n')
+            print(f'Weekly total....{sum(positSalcollectW)}\nWeekly Avg....{weeklyAVG}\n')
+            positSalcollectY.clear()
+            positSalcollectM.clear()
+            positSalcollectW.clear()
+            subretfunc(mgmtpositsal,nlist,retfunc)
+    else:
+        print('sorry There may not be anyone at that position. check the position directory. try again.')
+        mgmtpositsal(nlist,retfunc)
+        
+# this prints salary by position // prints mgmt as whole per dept        
+def positionSalprint(nlist,retfunc):
+    for salpos in range(len(positlistSTR)):
+        print(f'{salpos}....{positlistSTR[salpos]}')
+        
+    print(f'{len(positlistSTR)}....Management Listing by dep')
+    
+    userchoD = input('Please choose the salary position you wish to print: ')
+    
+    if userchoD in ('0123456789') and len(userchoD)>0 and (len(positlist[int(userchoD)])) > 0:
+        chospositdict = positlist[int(userchoD)]
+        for psal in chospositdict:
+            positSalcollectY.append(float(chospositdict[psal].yearlypay))
+            positSalcollectM.append(float(chospositdict[psal].monthlypay))
+            positSalcollectW.append(float(chospositdict[psal].weeklypay))
+        print(f'\n{positlistSTR[int(userchoD)]}....total count:{len(chospositdict)}')
+        yearlyAVG = float((sum((positSalcollectY))) // (len(positSalcollectY)))
+        monthlyAVG = float((sum((positSalcollectM))) // (len(positSalcollectM)))
+        weeklyAVG = float((sum((positSalcollectW))) // (len(positSalcollectW)))
+        print(f'Yearly total....{sum(positSalcollectY)}\nYearly Avg....{yearlyAVG}\n')
+        print(f'Monthly total....{sum(positSalcollectM)}\nMonthly Avg....{monthlyAVG}\n')
+        print(f'Weekly total....{sum(positSalcollectW)}\nWeekly Avg....{weeklyAVG}\n')      
+        positSalcollectY.clear()
+        positSalcollectM.clear()
+        positSalcollectW.clear()
+        subretfunc(positionSalprint,nlist,retfunc)
+    elif userchoD == '10' and len(userchoD)>0:
+        mgmtpositsal(nlist,retfunc)
+    else:
+        print('sorry There may not be anyone at that position. check the position directory. try again.')
+        positionSalprint(nlist,retfunc)
+
+    
+    
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# menu for departmental positional object viewer
+        
+def groupfuncMenu(nlist,retfunc):
+#     CHOICE 1 TO PRINT DIRECTORY RUNS SUBFUNC menuforDctry()
+    def menuforDctry(nlist,retfunc):
+        print('1....Complete Property Directory\n2....Department Directory\n3...Position Directory\n*...Previous Menu')
+        userchoB = input('Enter the no. of the directory you wish to open: ')
+        if userchoB in ('123*') and len(userchoB)>0:
+            if userchoB == '1':
+                propDirprint(nlist,retfunc)
+            elif userchoB =='2':
+                indyDirprint(nlist,retfunc)
+            elif userchoB =='3':
+                specDirprint(nlist,retfunc)
+            elif userchoB =='*':
+                groupfuncMenu(nlist,retfunc)
+            else:
+                menuforDctry(nlist,retfunc)
+        else:
+            print('Sorry Try again')
+            menuforDctry(nlist,retfunc)
+    
+    def menuforSalry(nlist,retfunc):
+        print('1...Department Salary Summary\n2...Position Salary Summary\n*...Previous Menu')
+        userchoD = input('Enter the no. of the Summary you wish to view: ')
+        if userchoD == '1':
+            depsalprint(nlist,retfunc)
+        elif userchoD =='2':
+            positionSalprint(nlist,retfunc)
+        elif userchoD =='*':
+            pass
+#             groupfuncMenu(nlist,retfunc)                
+        else:
+            print('Sorry Try again')
+            menuforSalry(nlist,retfunc)    
+            
+    gchoice = input('What would you like to do?\nTo print a Directory enter ... <a>\nTo print a Salary Summaries enter ... <b>\nTo see a count of each Property position enter ... <c>\nTo see a Department overview enter ... <d>\n')
+    if gchoice == 'a':
+        menuforDctry(nlist,retfunc)
+    elif gchoice == 'b':
+        menuforSalry(nlist,retfunc)
+    elif gchoice == 'c':
+        positCT(nlist,retfunc)
+    elif gchoice == 'd':   
+        depStat(nlist,retfunc)
+    elif gchoice == '*':
+        objectMenu(nlist,retfunc)
+    elif gchoice == 'q':
+        quit()
+    else:
+        print('please choose again')
+        groupfuncMenu(nlist,retfunc)
 
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -453,13 +867,24 @@ def quickempcheckMenu(nlist,retfunc):
 
 def objectMenu(nlist,retfunc):
     print('welcome to the dataviewer')
-    viewerchoice = input('To view individual Employee data enter ... < 1 >\nTo view Group date enter ... < 2 >\nTo return to main menu enter ... <q>\n:')
+    viewerchoice = input('To view individual Employee data enter ... < 1 >\nTo view Group date enter ... < 2 >\nTo return to main menu enter ... <*>\nTo QUIT enter ... <q>\n:')
     if viewerchoice == '1':
         quickempcheckMenu(nlist,retfunc)
     elif viewerchoice == '2':
-        pass
-    elif viewerchoice == 'q':
+        depsep('FD',fddict,FDposDict,FDposDictSTR,fdMgmt)
+        depsep('hsk',hskdict,HskposDict,HskposDictSTR,hskMgmt)
+        depsep('sle',sledict,SleposDict,SleposDictSTR,sleMgmt)
+        depsep('pbx',pbxdict,pbxposDict,pbxposDictSTR,pbxMgmt)
+        depsep('acct',accntdict,accntposDict,accntposDictSTR,accntMgmt)
+        depsep('sle',sledict,SleposDict,SleposDictSTR,sleMgmt)
+        depsep('eng',engdict,engposDict,engposDictSTR,engMgmt)
+        groupfuncMenu(nlist,retfunc)
+    elif viewerchoice == '*':
         retfunc()
+    elif viewerchoice == 't':
+        depStat()
+    elif viewerchoice == 'q':
+        quit
     else:
         print('please choose 1 or 2...try again')
         objectMenu(nlist,retfunc)
